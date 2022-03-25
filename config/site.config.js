814941a97a5e9d7794a540342d352abc71b85bc9
holdyounger <holdyounger@163.com>
2022-03-25 17:08:10 +0800

    :rocket: 修改密码保护文件

--- config/site.config.js
+++ config/site.config.js
@@ -43,7 +43,7 @@ module.exports = {
   protectedRoutes: [
     '/🌞 Private folder/u-need-a-password', 
     '/🥟 Some test files/Protected route',
-    '/Documents'
+    '/🎃 Documents'
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
